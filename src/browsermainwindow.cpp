d75f88ad42c4ed55eef1044f3761bedd47e3473d
Robert Hogan <robert@roberthogan.net>
2011-03-26 17:14:38 +0000

    Fix previous commit

--- src/browsermainwindow.cpp
+++ src/browsermainwindow.cpp
@@ -1582,9 +1582,9 @@ void BrowserMainWindow::loadProgress(int progress)
         updateStopReloadActionText(true);
     } else {
         disconnect(m_stopReloadAction, SIGNAL(triggered()), m_viewStopAction, SLOT(trigger()));
+        disconnect(m_stopReloadAction, SIGNAL(triggered()), this, SLOT(slotCheckTor()));
         m_stopReloadAction->setIcon(m_reloadIcon);
         if (!BrowserApplication::instance()->torManager()->usingTor()) {
-            disconnect(m_stopReloadAction, SIGNAL(triggered()), this, SLOT(slotCheckTor()));
             connect(m_stopReloadAction, SIGNAL(triggered()), this, SLOT(slotCheckTor()));
         } else
             connect(m_stopReloadAction, SIGNAL(triggered()), m_viewReloadAction, SLOT(trigger()));
